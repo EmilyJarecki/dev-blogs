506fdf535d71732e5bc864b0d964ae32a04e4765
EmilyJarecki <jareckiemily@yahoo.com>
2023-04-20 13:59:33 -0500

    changed routing to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,37 @@
-import type {NextPage} from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { InferGetStaticPropsType, NextPage } from "next";
+import BlogCard from "@/components/BlogCard";
+import { readPostsInfo } from "@/lib/helper";
+import { PostApiResponse } from "@/utils/types";
 
-const Home: NextPage=()=>{
-  return <h1 className="text-3xl font-bold underline text-gray-400">Hello World!</h1>
+
+
+export const getStaticProps = async()=>{
+    // const {postInfo}:PostApiResponse = await fetch(
+    //     "http://localhost:3000/api/posts"
+    // ).then((data)=>data.json());
+
+const postInfo: PostApiResponse = readPostsInfo()
+
+    return{
+        props: {posts: postInfo},
+    }
 }
 
-export default Home;
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+const Blogs: NextPage<Props> = ({posts})=>{
+    return(
+    <div className="max-w-3xl mx-auto p-5 space-y-5">
+        {posts.map((post)=>(
+            <BlogCard 
+                key={post.slug}
+                title={post.title}
+                desc={post.meta}
+                slug={post.slug}
+            />
+        ))}
+
+    </div>
+)
+}
+
+export default Blogs;
